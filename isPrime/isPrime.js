f9342750291039e65472c5105f65bc79c9c9f066
pornsitbureenok <ford@sennalabs.com>
2021-01-04 17:16:07 +0700

    task 7 : implement tested.

--- isPrime/isPrime.js
+++ isPrime/isPrime.js
@@ -1,6 +1,6 @@
 export function isPrime(num) {
-  if(num > 1){
-    throw new Error('Not Integer number');
+  if(num <= 1){
+    throw new Error('Not Integer number and more than 1');
   }
   const sqrtNum = Math.sqrt(num);
   for(let n = 2; n <= sqrtNum; n++)
